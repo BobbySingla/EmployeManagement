1bf182c4ede5ca3ac1407905d8392c8262110d31
bobby singla <bobbysingla1112@gmail.com>
2021-05-27 17:13:04 +0530

    Project Skeleton

---
+++ src/test/java/com/coders/employeManager/EmployeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.employeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
